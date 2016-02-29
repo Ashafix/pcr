37f4f647d4b82dcde7c0224a50a507586281fb0a
Ashafix <maximili.peters@mail.huji.ac.il>
2016-02-29 18:04:15 +0100

    added configuration file, rest server check, aws finder

--- cgi_bin/testserver.py
+++ cgi_bin/testserver.py
@@ -3,7 +3,8 @@
 #Tests if all the components of BatchPrimerDesign are working
 from subprocess import check_output, STDOUT
 from os import path
-from repeat_finder import read_configfile
+from repeat_finder import read_configfile, read_aws_conf
+import boto3
 
 def test_server(gfServer, servername, serverport):
 
@@ -23,10 +24,14 @@ def test_server(gfServer, servername, serverport):
 	else:
 		return False
 
-conf_arguments = read_configfile()
+config_filename = 'batchprimer.conf'
+conf_arguments = read_configfile(config_filename)
+
+remoteserver_url = ''
 
 message = ''
 message += 'Configuration file could '
+
 if conf_arguments == '':
 	message += 'NOT be read<br>'
 	message += 'NO further testing was possible to due a missing .conf file'
@@ -35,16 +40,56 @@ else:
 	message += str(conf_arguments) + '<br><br>'
 
 if conf_arguments != '':
-	message += 'gfServer is '
+	message += 'gfServer '
 	if test_server(conf_arguments['GFSERVER'], conf_arguments['SERVERNAME'], conf_arguments['SERVERPORT']):
-		message += 'up and running<br>'
+		message += 'is up and running<br>'
 	else:
-		message += 'IS NOT running<br>'
+		#check if it is possible to find an AWS server with the same name
+		session = boto3.session.Session(aws_access_key_id = aws['aws_access_key_id'], 
+			aws_secret_access_key = aws['aws_secret_access_key'], 
+			region_name = aws['region_name'])
+		ec2 = session.resource('ec2')
+		instances = ec2.instances.all()
+		for instance in instances:
+		instance_name = ''
+		for tag in instance.tags:
+			if 'Value' in tag and 'Key' in tag and instance_name == '':
+				if 'Key' in tag.keys() and 'Value' in tag.keys():
+					if tag['Key'] == 'Name': 
+						if tag['Value'] == config_args['SERVERNAME']:
+							if test_server(config_args['GFSERVER'], instance.private_dns_name.split('.')[0] , config_args['SERVERPORT']):
+								message += 'is up and running<br>'
+								config_args['SERVERNAME'] = instance.private_dns_name.split('.')[0]
+								instance_name = tag['Value']
+								remoteserver_url = 'http://' + instance.public_dns_name
+		if instance_name == '':
+			message += 'IS NOT running<br>'
+
 	message += 'Primer3 executable'
 	if path.isfile(conf_arguments['PRIMER3_EXE']):
 		message += ' is found<br>'
 	else:
 		message += ' IS NOT found<br>'
+	
+	message += 'REST server '
+	if remoteserver_url != '':
+		#checks if the REST server is responding
+		url = remoteserver_url + ':8003/cpuInfo'
+		reply = ''
+		try:
+			req = urllib2.Request(url, )
+			response = urllib2.urlopen(req)
+			reply = str(response.read())
+		except:
+			pass
+		if not 'CPU' in reply:
+			message += '<br>Rest server URL: ' + remoteserver_url
+			message += '<br>Error: Rest server is not responding'
+		else:
+			message += '<br>Rest server is responding correctly'
+	else:
+		message += '<br>Error: Remote server has no URL'
+
 
 message += '<form action="../primer.html">    <input type="submit" value="Go back to start page"></form>'
 
