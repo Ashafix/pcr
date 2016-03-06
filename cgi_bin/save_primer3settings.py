891006d96d6337303fc5a08d795a5981a9e3aec8
Ashafix <maximili.peters@mail.huji.ac.il>
2016-03-06 13:44:20 +0100

    first try of saving primer3 files

---
+++ cgi_bin/save_primer3settings.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+#creates an primer3 setting file from a cgi form
+
+import sys
+import cgi
+
+html = 'Content-Type:application/octet-stream\n'
+html += 'Content-Disposition: attachment; '
+html += 'filename="primer3settings.ini"\n\n'
+
+form = cgi.FieldStorage()
+for key in form.keys()  :
+	html += str(key)
+	html += '='
+	html += str(form[key].value)
+	
+
+print (html)
