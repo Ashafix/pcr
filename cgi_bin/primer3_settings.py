62ae35b957db4fca5c4b9f00fc348d75059d7fab
Ashafix <maximili.peters@mail.huji.ac.il>
2016-03-06 13:51:16 +0100

    output more flexible

--- cgi_bin/primer3_settings.py
+++ cgi_bin/primer3_settings.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+#creates an HTML input form for primer3 files
 
-#create an HTML input form for primer3 files
+import sys
 
+if len(sys.argv) == 1:
+	primer3_settings_filename = 'primer3_v1_1_4_default_settings.txt'
+	with open(primer3_settings_filename, 'r') as primer3_settings_file:
+		primer3_settings = primer3_settings_file.read()
+else:
+	primer3_settings_filename = sys.argv[1]
 
-primer3_settings = open('primer3_v1_1_4_default_settings.txt', 'r')
-
-
+print ('Content-Type: text/html')
 print ('<html><body>')
 
-for line in primer3_settings.readlines():
+for line in primer3_settings.split('\n'):
 	if '=' in line and line.startswith('PRIMER_'):
 		html = ''
 		html += '<li>'
@@ -28,7 +33,10 @@ for line in primer3_settings.readlines():
 	elif len(line.strip()) > 1:
 		print (line)
 		print ('<br>')
-primer3_settings.close()
 
+print ('<hr>')
+
+print ('<input type="submit" value="Save Primer3 parameters to file" name="SaveSettings" formaction="cgi-bin/save_primer3settings.py" />')
+print ('<input type="submit" value="Load Primer3 parameters from file" name="SaveSettings" formaction="cgi-bin/load_primer3settings.py" />')
 print ('</body>')
 print ('</html>')
