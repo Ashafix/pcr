5ee7d224f77887e226162d4ea781089f0b5b1e42
Ashafix <maximili.peters@mail.huji.ac.il>
2016-03-08 21:25:28 +0100

    fixed submit button

--- cgi_bin/primer3_settings.py
+++ cgi_bin/primer3_settings.py
@@ -6,16 +6,17 @@ import sys
 
 if len(sys.argv) == 1:
 	primer3_settings_filename = 'primer3_v1_1_4_default_settings.txt'
-	with open(primer3_settings_filename, 'r') as primer3_settings_file:
-		primer3_settings = primer3_settings_file.read()
 else:
 	primer3_settings_filename = sys.argv[1]
 
+with open(primer3_settings_filename, 'r') as primer3_settings_file:
+	primer3_settings = primer3_settings_file.read()
+
 html = 'Content-type:text/html; charset=utf-8\n\n'
 html += '<html><body>'
-
+html += '<form>'
 for line in primer3_settings.split('\n'):
-	if '=' in line and line.startswith('PRIMER_'):
+	if '=' in line and (line.startswith('PRIMER_') or line.startswith('P3_FILE_ID')):
 		html += '<li>'
 		html += '<label>'
 		html += line.split('=')[0]
@@ -30,18 +31,14 @@ for line in primer3_settings.split('\n'):
 		html += '</li>'
 	elif len(line.strip()) > 1:
 		html += '<br>\n'
-	
+
 html += '<hr>'
 
 html += '<input type="submit" value="Change Primer3 parameters" name="Primer3settings" formaction="../cgi-bin/save_primer3settings.py" />'
-html += '<a href="../cgi-bin/save_primer3settings.py"><img src="/images/Farm-Fresh_file_extension_zip.png"</a>'
+html += '</form>'
 html += '<a href="../cgi-bin/load_primer3settings.py">Upload</a>'
-
 html += '</body>'
 html += '</html>'
 
-
 sys.stdout.write(html)
 sys.stdout.flush()
-
-
