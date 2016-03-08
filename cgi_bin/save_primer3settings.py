f7be996f111c72714557cb83a05f57278b3fd4b2
Ashafix <maximili.peters@mail.huji.ac.il>
2016-03-08 21:25:37 +0100

    fixed ordered output

--- cgi_bin/save_primer3settings.py
+++ cgi_bin/save_primer3settings.py
@@ -9,10 +9,27 @@ html = 'Content-Type:application/octet-stream\n'
 html += 'Content-Disposition: attachment; '
 html += 'filename="primer3settings.ini"\n\n'
 
+if len(sys.argv) == 1:
+	primer3_settings_filename = 'primer3_v1_1_4_default_settings.txt'
+else:
+	primer3_settings_filename = sys.argv[1]
+with open(primer3_settings_filename, 'r') as primer3_settings_file:
+		primer3_settings = primer3_settings_file.read()
+
 form = cgi.FieldStorage()
-for key in form.keys()  :
-	html += str(key)
-	html += '='
-	html += str(form[key].value)
+
+html += 'Primer3 File - http://primer3.sourceforge.net\n'
+html += 'P3_FILE_TYPE=settings\n'
+
+for line in primer3_settings.split('\n'):
+	if '=' in line and (line.startswith('PRIMER_') or line.startswith('P3_FILE_ID')):
+		cells = line.split('=')
+		if len(cells) == 3:
+			if cells[0] in form.keys:
+				html += str(key)
+				html += '='
+				html += str(form[key].value)
+				html += '\n'
+html += '=\n'
 
 print (html)
