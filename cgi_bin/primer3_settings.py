c2ed7e71848f6c6907a1eecbe3ae4c4d43d96fcd
Ashafix <maximili.peters@mail.huji.ac.il>
2019-04-25 20:58:39 +0200

    cleaned primer3_settings script

--- cgi_bin/primer3_settings.py
+++ cgi_bin/primer3_settings.py
@@ -1,45 +1,48 @@
 #!/usr/bin/env python
 
-#creates an HTML input form for primer3 files
-
 import sys
 
-if len(sys.argv) == 1:
-	primer3_settings_filename = 'primer3_settingsDavid.txt'
-else:
-	primer3_settings_filename = sys.argv[1]
 
-with open(primer3_settings_filename, 'r') as primer3_settings_file:
-	primer3_settings = primer3_settings_file.read()
+def main(args):
+    # creates an HTML input form for primer3 files
 
-html = 'Content-type:text/html; charset=utf-8\n\n'
+    if len(args) == 1:
+        primer3_settings_filename = 'primer3_settingsDavid.txt'
+    else:
+        primer3_settings_filename = args[1]
 
-html += '<html><head> <link rel="stylesheet" type="text/css" href="../html/primer3_settings.css" /> </head><body>'
-html += '<div class="container"><form>'
-for line in primer3_settings.split('\n'):
-	if '=' in line and (line.startswith('PRIMER_') or line.startswith('P3_FILE_ID')):
-		html += '<li>'
-		html += '<label>'
-		html += line.split('=')[0]
-		html += '</label>'
-		html += '<input name="'
-		html += line.split('=')[0]
-		html += '"'
-		html += ' value="'
-		html += line.split('=')[1].strip()
-		html += '"'
-		html += ' />'
-		html += '</li>'
-		html += '\n'
-	#elif len(line.strip()) > 1:
-		#html += '<br>\n'
+    with open(primer3_settings_filename, 'r') as primer3_settings_file:
+        primer3_settings = primer3_settings_file.read()
 
-html += '<hr>'
+    html = 'Content-type:text/html; charset=utf-8\n\n'
 
-html += '<input type="submit" value="Download Primer3 parameters" name="Primer3settings" formaction="../cgi-bin/save_primer3settings.py" />'
-html += '</form></div>'
-html += '</body>'
-html += '</html>'
+    html += '<html><head> <link rel="stylesheet" type="text/css" href="../html/primer3_settings.css" /> </head><body>'
+    html += '<div class="container"><form>'
+    for line in primer3_settings.split('\n'):
+        if '=' in line and (line.startswith('PRIMER_') or line.startswith('P3_FILE_ID')):
+            html += '<li>'
+            html += '<label>'
+            html += line.split('=')[0]
+            html += '</label>'
+            html += '<input name="'
+            html += line.split('=')[0]
+            html += '"'
+            html += ' value="'
+            html += line.split('=')[1].strip()
+            html += '"'
+            html += ' />'
+            html += '</li>'
+            html += '\n'
 
-sys.stdout.write(html)
-sys.stdout.flush()
+    html += '<hr>'
+    html += '<input type="submit" value="Download Primer3 parameters" name="Primer3settings" formaction="../cgi-bin/save_primer3settings.py" />'
+    html += '</form></div>'
+    html += '</body>'
+    html += '</html>'
+
+    sys.stdout.write(html)
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main(sys.args)
